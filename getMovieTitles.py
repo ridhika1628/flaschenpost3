40cde6f1cbd002aabb584b5bd59bb5ff58e80be1
ridhika1628 <65011651+ridhika1628@users.noreply.github.com>
2022-09-22 01:46:07 +0530

    Update python file with comments

--- getMovieTitles.py
+++ getMovieTitles.py
@@ -1,13 +1,19 @@
+# import requests and json module
 import requests
 import json
 
 
 def getMovieTitles(substr):
     titles = []
+    # Making a GET request
     data = requests.get("https://jsonmock.hackerrank.com/api/movies/search/?Title= {}".format(substr))
+    # check status code for response received
+    # success code - 200
     #print(data)
+    # Json content
     response = json.loads(data.content.decode('utf-8'))
     total_page = response["total_pages"]
+    # print content of request
     #print(data.content)
     for page in range(0, response["total_pages"]):
         page_response = requests.get("https://jsonmock.hackerrank.com/api/movies/search/?Title={}&page={}".format(substr, page + 1))
@@ -15,6 +21,7 @@ def getMovieTitles(substr):
         #print ('page_content', page_content, 'type(page_content)', type(page_content))
         for item in range(0, len(page_content["data"])):
             titles.append(str(page_content["data"][item]["Title"]))
-    titles.sort()    
+    titles.sort()
+    # Return statement to send the titles result back after sorting the Titles
     return titles
 print(getMovieTitles("Superman"))
